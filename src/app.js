d08c57b2d67d32498097267fe53670be2d6755e5
Sunil <sunildarji811@gmail.com>
2020-08-20 22:24:25 +0530

    app.js

--- src/app.js
+++ src/app.js
@@ -20,16 +20,38 @@ const app = express();
 app.use(cors());
 
 app.use((req, res, next) => {
-  bodyParser.json()(req, res, (err) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+  /*
+    bodyParser.json()(req, res, (err) => {
     if (err) {
       return res.sendStatus(400); // Bad request
     }
-
-    next();
   });
+  */
+    next();
 });
 
-app.use(express.json());
+app.use(bodyParser.urlencoded({
+  limit: '50mb',
+  extended: true
+}));
+
+app.use(bodyParser.json({
+  limit: '50mb'
+}));
+
+//app.use(bodyParser.raw());
+//app.use(express.json());
 app.use(morgan('combined', { stream: winston.stream }));
 app.use(userRouter);
 app.use("/instagram", instagramRouter);
